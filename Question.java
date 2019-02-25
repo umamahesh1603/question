0c7055e82a989065fd59f900fb0f0a52e92a2bfa
umamahesh1603 <umamaheshbondalapati8057@gmail.com>
2019-02-25 12:26:28 +0530

    question adding

---
+++ Question.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+
+
+public class Question
+{
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        while(true){
+            System.out.println("Hello, World");
+        }
+    }
+
+    }
